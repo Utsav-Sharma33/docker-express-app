11ca4a20bb8a25a7a873cc9d6ad8e959eff71c57
Utsav-Sharma33 <utsav.sharma56@gmail.com>
2021-05-06 20:46:20 +0530

    added rest client and started host service

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const express = require('express');
 const app = express();
 
 
-app.use("/", (req, res, next) => {
-    res.render("<h3>App started succesfully</h3>")
+app.get("/", (req, res, next) => {
+    res.send("<h3>App started succesfully</h3>")
 })
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log("app started on port", process.env.PORT)
+    console.log(`app started on port ${port}`)
 })
+
+app.on('error', console.log)
